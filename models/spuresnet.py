06757babf712ccdc682c93104f533487a23cd273
twongjirad <taritree.wongjirad@gmail.com>
2019-02-20 15:34:36 -0500

    larcv bumped for img2d to pixellist. also drafts of sparse model

---
+++ models/spuresnet.py
@@ -0,0 +1,62 @@
+import os,sys
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import sparseconvnet as scn
+import time
+import math
+import numpy as np
+
+#data.init(-1,24,24*8,16)
+#dimension = 3
+#reps = 1 #Conv block repetition factor
+#m = 32   #Unet number of features
+#nPlanes = [m, 2*m, 3*m, 4*m, 5*m] #UNet number of features per level
+
+class SparseUResNet(nn.Module):
+    def __init__(self, inputshape, reps, nfeatures, nplanes, noutput_classes):
+        nn.Module.__init__(self)
+
+        # set parameters
+        self.dimensions = 2 # not playing with 3D for now
+
+        # input shape: LongTensor, tuple, or list. Handled by InputLayer
+        # size of each spatial dimesion
+        self.inputshape = inputshape
+        if len(self.inputshape)!=self.dimensions:
+            raise ValueError("expected inputshape to contain size of 2 dimensions only. given %d values"%(len(self.inputshape)))
+        
+        # mode variable: how to deal with repeated data
+        self.mode = 3
+
+        # nfeatures
+        self.nfeatures = nfeatures
+
+        # plane structure
+        self.nPlanes = [ self.nfeatures*(n+1) for n in xrange(nplanes) ]
+
+        # repetitions (per plane)
+        self.reps = reps
+
+        # output classes
+        self.noutput_classes = noutput_classes
+        
+        # model:
+        # input
+        # stem
+        # unet
+        # linear to nclasses
+        self.sparseModel = scn.Sequential().add(
+            scn.InputLayer(self.dimensions, self.inputshape, mode=self.mode)).add(
+            scn.SubmanifoldConvolution(self.dimensions, 1, self.nfeatures, 3, False)).add(
+            scn.UNet(self.dimensions, self.reps, self.nPlanes, residual_blocks=True, downsample=[2,2])).add(
+            scn.BatchNormReLU(self.nfeatures)).add(
+            scn.OutputLayer(self.dimensions))
+        self.linear = nn.Linear(self.nfeatures, self.noutput_classes)
+        
+    def forward(self,x):
+        x=self.sparseModel(x)
+        x=self.linear(x)
+        return x
